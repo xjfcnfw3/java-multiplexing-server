494754a1b8e282898da9276e8dc383aafe266bb2
kimminsu <xjfcnfw3@naver.com>
2025-01-04 19:30:34 +0900

    ✨ feat: 클라이언트 메시지 에코

--- src/main/java/server/BaseIOServer.java
+++ src/main/java/server/BaseIOServer.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import socket.BaseIOSocket;
 
 public class BaseIOServer implements Server {
@@ -12,7 +14,7 @@ public class BaseIOServer implements Server {
     @Override
     public void run(String host, int port) {
         try (ServerSocket serverSocket = new ServerSocket()) {
-            serverSocket.bind(new InetSocketAddress("localhost", 8080), 500);
+            serverSocket.bind(new InetSocketAddress("localhost", 8080));
             runServer(serverSocket);
         } catch (IOException e) {
             e.printStackTrace();
@@ -20,11 +22,14 @@ public class BaseIOServer implements Server {
     }
 
     private void runServer(ServerSocket serverSocket) {
+        ExecutorService executorService = Executors.newFixedThreadPool(500);
+        System.out.println("start Server");
         while (true) {
             try {
                 Socket connection = serverSocket.accept();
+                connection.setSoTimeout(5000);
                 BaseIOSocket socket = new BaseIOSocket(connection);
-                socket.start();
+                executorService.execute(socket::start);
             } catch (SocketException e) {
                 e.printStackTrace();
             } catch (IOException e) {
--- src/main/java/socket/BaseIOSocket.java
+++ src/main/java/socket/BaseIOSocket.java
@@ -1,8 +1,12 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseIOSocket extends Thread {
 
@@ -17,16 +21,60 @@ public class BaseIOSocket extends Thread {
     public void run() {
         if (socket.isConnected()) {
             try {
-                PrintStream response = new PrintStream(socket.getOutputStream());
-                response.println("HTTP/1.1 200 OK");
-                response.println("Content-type: text/html");
-                response.println();
-                response.print("It is working.");
-                response.flush();
-                response.close();
+                String body = getRequestBody();
+                echoMessage(body);
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException ignored) {}
             }
         }
+    }
+
+    private void echoMessage(String body) throws IOException {
+        PrintStream response = new PrintStream(socket.getOutputStream());
+        response.println("HTTP/1.1 200 OK");
+        response.println("Content-type: text/html");
+        response.println();
+        response.print(body);
+        response.flush();
+        response.close();
+    }
+
+    private String getRequestBody() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Map<String, String> startLine = getStartLine(bufferedReader);
+        Map<String, String> headers = getHeaders(bufferedReader);
+        return getBody(bufferedReader, headers);
+    }
+
+    private String getBody(BufferedReader bufferedReader, Map<String, String> header) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        int length = Integer.parseInt(header.get("Content-Length"));
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append((char) bufferedReader.read());
+        }
+        return stringBuilder.toString();
+    }
+
+    private Map<String, String> getStartLine(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> startLine = new HashMap<>();
+        String[] values = bufferedReader.readLine().split(" ", 3);
+        startLine.put("METHOD", values[0]);
+        startLine.put("URI", values[1]);
+        startLine.put("VERSION", values[2]);
+        return startLine;
+    }
+
+    private Map<String, String> getHeaders(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String header;
+        while ((header = bufferedReader.readLine()) != null && !header.isEmpty()) {
+            String[] headerKeyValue = header.split(": ", 2);
+            headers.put(headerKeyValue[0], headerKeyValue[1]);
+        }
+        return headers;
     }
 }
